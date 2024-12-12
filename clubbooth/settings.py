c90e5a2e242c021496bcc7031a13f57b40c8f416
taehyun0411 <129483733+taehyun07@users.noreply.github.com>
2024-12-12 10:18:18 +0900

    static update

--- clubbooth/settings.py
+++ clubbooth/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.template.context_processors import static
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "main"
 ]
 
 MIDDLEWARE = [
@@ -117,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'aptitude', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ main/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include
+from main.views import index
+urlpatterns = [
+    path("", index, name="index"),
+]
