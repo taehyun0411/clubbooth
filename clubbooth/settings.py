3fe9805f64282dcad3e54a4de59fe5966196f03c
taehyun0411 <65107935+taehyun0411@users.noreply.github.com>
2024-12-18 23:13:01 +0900

    Update settings.py
    test

--- clubbooth/settings.py
+++ clubbooth/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-geipvhi7n6)0urn*)(tar*^i&9z&sjgdg&@hegdum83h@dlotr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['semicolon-clubbooth.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
